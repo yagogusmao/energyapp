96a5779d1ee3ba5a52aef803ceb441c668e009ff
Yago <yagogusmao1998@gmail.com>
2020-05-09 11:32:35 -0300

    fix app js with cors

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var app = express();
+
+const corsOptions = {
+  origin: '*',
+  methods: [
+    'GET',
+    'PUT',
+    'POST',
+    'DELETE'
+  ],
+  allowedHeaders: [
+    'Content-Type',
+  ]
+}
+
+app.use(cors(corsOptions));
 
 const port = process.env.PORT || 8080;
 const config = require('./src/config/config');
@@ -21,12 +37,12 @@ const AtividadeController = require('./src/controllers/AtividadeController');
 app.use('/atividade', AtividadeController)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -36,7 +52,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("App is running on port " + port);
 });
 
