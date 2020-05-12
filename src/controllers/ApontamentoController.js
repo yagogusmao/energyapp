99427c2d55b1fb139012d9e90af71a18f02ded15
Yago <yagogusmao1998@gmail.com>
2020-05-12 14:25:01 -0300

    ...

--- src/controllers/ApontamentoController.js
+++ src/controllers/ApontamentoController.js
@@ -4,21 +4,44 @@ const router = express.Router();
 const Apontamento = require('../models/Apontamento');
 
 router.route('/')
+    .get((req, res) => {
+        Apontamento.find().then(apontamentos => res.status(200).json({ sucesso: true, messagem: "Apontamentos cadastrados no sistema.", apontamentos }))
+    })
+
+router.route('/iniciar')
     .post((req, res) => {
-        try{
-            const {tipo, pessoa, veiculo, PgCp, equipe, cidade, endereco, hora, local, atividades} = req.body;
+        try {
+            const { tipo, pessoaSupervisor, pessoaEncarregado, veiculoPlaca, veiculoKmInicio, veiculoSi, equipe, cidade, endereco, localSaida } = req.body;
             let apontamento = new Apontamento();
-            apontamento.criar(tipo, pessoa, veiculo, PgCp, equipe, cidade, endereco, hora, local, atividades);
+            apontamento.iniciar(tipo, pessoaSupervisor, pessoaEncarregado, veiculoPlaca, veiculoKmInicio, veiculoSi, equipe, cidade, endereco, localSaida);
             apontamento.save((erro, apontamento) => {
-                if (!erro) res.status(201).json({sucesso: true, messagem: "Apontamento criado com sucesso.", apontamento});
-                else res.status(400).json({sucesso: false, messagem: erro});
+                if (!erro) res.status(201).json({ sucesso: true, messagem: "Apontamento criado com sucesso.", apontamento });
+                else res.status(400).json({ sucesso: false, messagem: erro });
             })
         } catch (erro) {
-            res.status(400).json({ sucesso: false, messagem: erro});
+            res.status(400).json({ sucess: false, messagem: erro.message })
         }
     })
-    .get((req, res) => {
-        Apontamento.find().then(apontamentos => res.status(200).json({sucesso: true, messagem: "Apontamentos cadastrados no sistema.", apontamentos}))
+
+router.route('/finalizar')
+    .post((req, res) => {
+        try {
+            const { _id, tecnicoEnergisa, veiculoKmFim, PgCp, atividades } = req.body;
+            Apontamento.findById(_id).then(apontamento => {
+                if (apontamento) {
+                    if (apontamento.status === "INICIADO") {
+                        apontamento.finalizar(tecnicoEnergisa, veiculoKmFim, PgCp, atividades).then(() => {
+                            apontamento.save((erro, apontamento) => {
+                                if (!erro) res.status(200).json({ sucesso: true, messagem: "Apontamento finalizado com sucesso.", apontamento });
+                                else res.status(400).json({ sucesso: false, messagem: erro });
+                            })
+                        }).catch(erro => res.status(400).json({ sucesso: false, messagem: erro }))
+                    } else res.status(400).json({ sucesso: false, messagem: "Apontamento já finalizado." })
+                } else res.status(400).json({ sucesso: false, messagem: "Apontamento não encontrado." });
+            })
+        } catch (erro) {
+            res.status(400).json({ sucesso: false, messagem: erro.message })
+        }
     })
 
 module.exports = router;
--- src/models/Apontamento.js
+++ src/models/Apontamento.js
@@ -3,47 +3,51 @@ const Schema = moongose.Schema;
 const Atividade = require('../models/Atividade');
 
 const ApontamentoSchema = new Schema({
-    tipo: {type: String, enum: ["MANUTENCAO", "CONSTRUCAO", "OPERACIONAL", "PERDAS", "LINHAVIVA"], required: true},
+    tipo: { type: String, enum: ["MANUTENCAO", "CONSTRUCAO", "OPERACIONAL", "PERDAS", "LINHAVIVA"], required: true },
     pessoa: {
-        supervisor: {type: String, required: true},
+        supervisor: { type: String, required: true },
         tecnicoEnergisa: String,
-        encarregado: {type: String, required: true},
+        encarregado: { type: String, required: true },
     },
     veiculo: {
-        placa: {type: String, required: true},
+        placa: { type: String, required: true },
         kilometragem: {
-            inicio: {type: Number, required: true},
+            inicio: { type: Number, required: true },
             fim: Number,
             total: Number
         },
-        si: {type: String, required: true}
+        si: { type: String, required: true }
     },
     PgCp: String,
-    equipe: {type: String, required: true},
-    cidade: {type: String, required: true},
-    endereco: {type: String, required: true},
+    equipe: { type: String, required: true },
+    cidade: { type: String, required: true },
+    endereco: { type: String, required: true },
+    local: {
+        saida: { type: String, required: true },
+        chegada: { type: String, required: true }
+    },
     hora: {
-        inicio: {type: Date, required: true},
+        inicio: { type: Date, required: true },
         fim: Date
     },
-    atividades: [{
-        _id: {type: String, ref: 'Atividade'},
-        quantidade: Number
-    }],
-    status: {type: String, enum: ["INICIADO", "FINALIZADO"], required: true}
+    atividades: [],
+    lucro: Number,
+    status: { type: String, enum: ["INICIADO", "FINALIZADO"], required: true }
 });
 
-ApontamentoSchema.methods.iniciar = (tipo, pessoaSupervisor, pessoaEncarregado, veiculoPlaca, veiculoKmInicio, equipe, cidade, endereco, horaInicio, localSaida) => {
+ApontamentoSchema.methods.iniciar = function iniciar(tipo, pessoaSupervisor, pessoaEncarregado, veiculoPlaca, veiculoKmInicio, veiculoSi, equipe, cidade, endereco, localSaida) {
     this.tipo = tipo;
     this.pessoa.supervisor = pessoaSupervisor;
     this.pessoa.encarregado = pessoaEncarregado;
     this.veiculo.placa = veiculoPlaca;
     this.veiculo.kilometragem.inicio = veiculoKmInicio;
+    this.veiculo.si = veiculoSi;
     this.equipe = equipe;
     this.cidade = cidade;
     this.endereco = endereco;
-    this.hora.inicio = horaInicio;
+    this.hora.inicio = new Date();
     this.local.saida = localSaida;
+    this.local.chegada = `Cidade: ${this.cidade}, Endereço: ${this.endereco}`;
     this.status = "INICIADO";
 }
 
@@ -51,23 +55,35 @@ ApontamentoSchema.methods.atualizar = () => {
 
 }
 
-ApontamentoSchema.methods.finalizar = (tecnicoEnergisa, veiculoKmFim, PgCp, horaFim, atividades) => {
-    this.pessoa.tecnicoEnergisa = tecnicoEnergisa;
-    this.veiculo.kilometragem.fim = veiculoKmFim;
-    this.veiculo.kilometragem.total = this.veiculo.kilometragem.fim - this.veiculo.kilometragem.inicio; 
-    this.PgCp = PgCp;
-    this.hora.fim = horaFim;
-    this.atividades = atividades;
-    this.status = "FINALIZADO"
+ApontamentoSchema.methods.finalizar = function finalizar(tecnicoEnergisa, veiculoKmFim, PgCp, atividades) {
+    return validarAtividades(atividades).then(lucro => {
+        this.lucro = lucro;
+        this.pessoa.tecnicoEnergisa = tecnicoEnergisa;
+        this.veiculo.kilometragem.fim = veiculoKmFim;
+        this.veiculo.kilometragem.total = this.veiculo.kilometragem.fim - this.veiculo.kilometragem.inicio;
+        this.PgCp = PgCp;
+        this.hora.fim = new Date();
+        this.status = "FINALIZADO";
+        this.atividades = atividades;
+    }).catch(erro => { throw erro })
+
 }
 
-ApontamentoSchema.methods.pegarInformacoes = async () => {
-    let total = 0;
-    this.atividades.forEach(atividade => {
-        await Atividade.findById(atividade._id).then(atividade => {
-            total += atividade.valor;
-        })
-    })
+const validarAtividades = (atividades) => {
+    let promessas = atividades.map(atividade => Atividade.findById(atividade._id));
+    return Promise.all(promessas).then(promessaAtividades => {
+        if (!promessaAtividades.includes(null)) {
+            let total = 0;
+            promessaAtividades.forEach((atividade, i) => { total += atividade.valor * atividades[i].quantidade })
+            return total;
+        } else {
+            let str = "";
+            promessaAtividades.forEach((atividade, i) => {
+                if (atividade === null) str += (atividades[i]._id + " ");
+            })
+            throw `A(s) atividade(s): ${str}não estão no sistema.`;
+        }
+    });
 }
 
 module.exports = moongose.model('Apontamento', ApontamentoSchema);
