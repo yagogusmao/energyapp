21034741a8ca0309d787c7f11fb779b4a60c98dc
Yago <yagogusmao1998@gmail.com>
2020-06-02 17:51:21 -0300

    iniciando relatórios

--- src/controllers/AlmoxarifadoController.js
+++ src/controllers/AlmoxarifadoController.js
@@ -5,6 +5,12 @@ const queryString = require('query-string');
 const Almoxarifado = require('../models/Almoxarifado');
 
 router.route('/')
+    /**
+     * post example localhost:8080/almoxarifado
+    {
+        "_id": "CAMPINAGRANDE-EPB"
+    }
+     */
     .post((req, res) => {
         const { _id } = req.body;
         let almoxarifado = new Almoxarifado();
@@ -14,6 +20,9 @@ router.route('/')
             else res.status(201).json({ sucesso: true, mensagem: "Almoxarifado criado com sucesso.", almoxarifado })
         })
     })
+    /**
+     * get example localhost:8080/almoxarifado
+     */
     .get((req, res) => {
         Almoxarifado.find().then(almoxarifados => res.status(200).json({
             sucesso: true,
@@ -22,6 +31,9 @@ router.route('/')
     })
 
 router.route('/estoque')
+    /**
+     * get example localhost:8080/almoxarifado/estoque?_id=<_id do almoxarifado>
+     */
     .get((req, res) => {
         const { _id } = queryString.parse(req._parsedUrl.query);
         Almoxarifado.findById(_id).then(almoxarifado => {
@@ -33,11 +45,20 @@ router.route('/estoque')
             else res.status(400).json({ sucesso: false, mensagem: "Almoxarifado não encontrado." });
         })
     })
+    /**
+     * put example localhost:8080/almoxarifado/estoque
+    {
+        "_id": "CAMPINAGRANDE-EPB",
+        "material": "20",
+        "quantidade": 40,
+        "vemDe": "ENERGISA"
+    }
+     */
     .put((req, res) => {
-        const { _id, material, quantidade } = req.body;
+        const { _id, material, quantidade, vemDe } = req.body;
         Almoxarifado.findById(_id).then(almoxarifado => {
             if (almoxarifado) {
-                almoxarifado.adicionar(material, quantidade).then(() => {
+                almoxarifado.adicionar(material, quantidade, vemDe).then(() => {
                     almoxarifado.save((erro, almoxarifado) => {
                         if (erro) res.status(400).json({ sucesso: false, mensagem: erro.message });
                         else almoxarifado.verEstoque().then(materiais => res.status(200).json({
@@ -49,12 +70,19 @@ router.route('/estoque')
             } else res.status(400).json({ sucesso: false, mensagem: "Almoxarifado não encontrado." });
         })
     })
+    /**
+     * delete example localhost:8080/almoxarifado/estoque?_id=<_id do almoxarifado>&
+        material=<_id do material>&
+        quantidade=<quantidade de material que vai sair>&
+        vaiPara=<local para onde vai o material>&
+        servico=<código do servico>
+     */
     .delete((req, res) => {
-        const { _id, material, quantidade } = queryString.parse(req._parsedUrl.query);
+        const { _id, material, quantidade, vaiPara, servico, equipe } = queryString.parse(req._parsedUrl.query);
         Almoxarifado.findById(_id).then(almoxarifado => {
             if (almoxarifado) {
                 try {
-                    almoxarifado.retirar(material, quantidade).then(() => {
+                    almoxarifado.retirar(material, quantidade, vaiPara, servico, equipe).then(() => {
                         almoxarifado.save((erro, almoxarifado) => {
                             if (erro) res.status(400).json({ sucesso: false, mensagem: erro.message });
                             else almoxarifado.verEstoque().then(materiais => res.status(200).json({
@@ -65,6 +93,22 @@ router.route('/estoque')
                     }).catch(erro => res.status(400).json({ sucesso: false, mensagem: erro + "" }));
                 } catch (erro) { res.status(400).json({ sucesso: false, mensagem: erro + "" }) }
             } else res.status(400).json({ sucesso: false, mensagem: "Almoxarifado não encontrado." });
+        })
+    })
+
+router.route('/relatorio')
+    /**
+    * get example localhost:8080/almoxarifado/relatorio?_id=<_id do almoxarifado>&opcao=<entrada ou saida>
+    */
+    .get((req, res) => {
+        const { _id, opcao } = queryString.parse(req._parsedUrl.query);
+        Almoxarifado.findById(_id).then(almoxarifado => {
+            if (almoxarifado) almoxarifado.verRelatorio(opcao)
+                .then(relatorio => res.status(200).json({
+                    sucesso: true,
+                    mensagem: "Relatório retirado com sucesso.", relatorio
+                })).catch(erro => res.status(400).json({ sucesso: false, mensagem: erro + "" }))
+            else res.status(400).json({ sucesso: false, mensagem: "Almoxarifado não encontrado." });
         })
     })
 module.exports = router;
--- src/models/Almoxarifado.js
+++ src/models/Almoxarifado.js
@@ -9,8 +9,8 @@ const AlmoxarifadoSchema = new Schema({
             'SUME-EPB', 'ESPERANCA-EPB', 'SOLANEA-EPB', 'GUARABIRA-EPB'], required: true
     },
     estoque: { type: Map, of: Number, required: true }, //_id do material aponta pra a quantidade do material
-    entradas: [{ material: String, quantidade: Number, vemDe: String }],
-    saidas: [{ material: String, quantidade: Number, vaiPra: String }]
+    entradas: [{ material: String, quantidade: Number, vemDe: String, data: Date }],
+    saidas: [{ material: String, quantidade: Number, vaiPara: String, servico: String, data: Date, equipe: String }]
 });
 
 AlmoxarifadoSchema.methods.criar = function criar(_id) {
@@ -18,23 +18,27 @@ AlmoxarifadoSchema.methods.criar = function criar(_id) {
     this.estoque = new Map();
 }
 
-AlmoxarifadoSchema.methods.adicionar = function adicionar(materialId, quantidade) {
+AlmoxarifadoSchema.methods.adicionar = function adicionar(materialId, quantidade, vemDe) {
     if (this.estoque.has(materialId)) {
         const novaQuantidade = quantidade + this.estoque.get(materialId);
-        this.estoque.set(materialId, novaQuantidade)
+        this.estoque.set(materialId, novaQuantidade);
+        this.entradas.push({ material: materialId, quantidade, vemDe, data: new Date() })
         return Promise.resolve();
     } else return Material.findById(materialId).then(material => {
-        if (material) this.estoque.set(materialId, quantidade)
-        else throw "Material não encontrado.";
+        if (material) {
+            this.estoque.set(materialId, quantidade);
+            this.entradas.push({ material: materialId, quantidade, vemDe, data: new Date() })
+        } else throw "Material não encontrado.";
     })
 }
 
-AlmoxarifadoSchema.methods.retirar = function retirar(materialId, quantidade) {
+AlmoxarifadoSchema.methods.retirar = function retirar(materialId, quantidade, vaiPara, servico, equipe) {
     if (this.estoque.has(materialId)) {
         if (this.estoque.get(materialId) - quantidade < 0) throw "O material ainda não possui estoque.";
         else {
             const novaQuantidade = this.estoque.get(materialId) - quantidade;
             this.estoque.set(materialId, novaQuantidade)
+            this.saidas.push({ material: materialId, quantidade, vaiPara, servico, data: new Date(), equipe })
             return Promise.resolve();
         };
     } else throw "Material não encontrado no estoque.";
@@ -52,6 +56,39 @@ AlmoxarifadoSchema.methods.verEstoque = async function verEstoque() {
                 }
         })
         )
+}
+
+AlmoxarifadoSchema.methods.verRelatorio = async function verRelatorio(opcao) {
+    if (opcao === "saida") {
+        return await Promise.all(this.saidas.map(saida => Material.findById(saida.material)))
+            .then(materiais => this.saidas.map((saida, i) => {
+                if (materiais[i]._id == saida.material)
+                    return {
+                        _id: materiais[i]._id,
+                        descricao: materiais[i].descricao,
+                        quantidade: saida.quantidade,
+                        unidadeMedida: materiais[i].unidadeMedida,
+                        vaiPara: saida.vaiPara,
+                        data: saida.data,
+                        servico: saida.servico
+                    }
+            })
+            )
+    } else if (opcao === "entrada") {
+        return await Promise.all(this.entradas.map(entrada => Material.findById(entrada.material)))
+            .then(materiais => this.entradas.map((entrada, i) => {
+                if (materiais[i]._id == entrada.material)
+                    return {
+                        _id: materiais[i]._id,
+                        descricao: materiais[i].descricao,
+                        quantidade: entrada.quantidade,
+                        unidadeMedida: materiais[i].unidadeMedida,
+                        vemDe: entrada.vemDe,
+                        data: entrada.data
+                    }
+            })
+            )
+    } else throw "Defina uma opção válida.";
 }
 
 module.exports = moongose.model('Almoxarifado', AlmoxarifadoSchema);
